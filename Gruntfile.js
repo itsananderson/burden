74e1cdf3ed76fb1163f8bed255d39fbd4b9b06e5
Will Anderson <will@itsananderson.com>
2014-04-14 23:48:53 -0700

    Move tasks into tasks folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,24 +8,7 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks("grunt-beep");
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadTasks("./tasks");
 
-    grunt.registerTask('generate', 'Generate blog from static files', function() {
-        console.log('generating..');
-    });
-
-    grunt.registerTask('preview', 'Preview generated blog', function() {
-        console.log('previewing...');
-    });
-
-    grunt.registerTask('push', 'Push generated site to the remote', function() {
-        console.log('pushing...');
-    });
-
-    grunt.registerTask('publish', 'Generate and push the site', ['generate', 'push']);
-
-    grunt.registerTask('fail', function() {
-        console.log('failing...');
-        return false;
-    });
 };
