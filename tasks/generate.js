ffc2fa5c8ec223ce0325c0d10dcb7dd8d6bd129e
unknown <wiand@wiand-laptop.redmond.corp.microsoft.com>
2014-05-12 08:20:02 -0700

    Snapshot

---
+++ tasks/generate.js
@@ -0,0 +1,6 @@
+"use strict";
+module.exports = function(grunt) {
+    grunt.registerTask('generate', 'Generate blog from static files', function() {
+        console.log('generating..');
+    });
+};
---
+++ tasks/push.js
@@ -0,0 +1,6 @@
+"use strict";
+module.exports = function(grunt) {
+    grunt.registerTask('push', 'Push generated site to the remote', function () {
+        console.log('pushing...');
+    });
+};
